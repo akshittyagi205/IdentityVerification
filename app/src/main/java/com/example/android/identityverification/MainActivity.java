72a7d05ba92cb8e7cf841bdc1fba2ea1adaaa08b
Subrat <connect2subrat@gmail.com>
2018-02-17 15:41:17 +0530

    Project started and configured!

--- app/src/main/java/com/example/android/identityverification/MainActivity.java
+++ app/src/main/java/com/example/android/identityverification/MainActivity.java
@@ -3,11 +3,24 @@ package com.example.android.identityverification;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*
+* Some tips:
+* Same file pr kbhi ek sath kaam nhi krna
+* push and pull k liye ye upr do arrows jo h wo use krne h
+* push and pull ka mtlb to pta hi hoga
+* jb b kuch kaam kro to push krdo
+* and humehsa kaam shuru krne se pehle pull krlo
+* push krke b dikha dun ek baar?
+* dikah de
+* push krne k liye kuch change krna zaroori hai
+* */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Suppose ye change hai
     }
 }
