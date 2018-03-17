af6a649a543a5f6d862c9cf53e3d0f094745e23b
Subrat <connect2subrat@gmail.com>
2018-03-17 13:25:55 +0530

    select company

---
+++ app/src/main/java/com/example/android/identityverification/E_KYC_HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.identityverification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class E_KYC_HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_e_kyc_home);
+    }
+}
