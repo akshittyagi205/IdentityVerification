d276f7cce51925bba8f91f9a3cdcafa715febb2e
akshit20596 <a.tyagi20596@gmail.com>
2018-03-21 00:02:31 +0530

    Home Fragment added

--- app/src/main/java/com/example/android/identityverification/MainActivity.java
+++ app/src/main/java/com/example/android/identityverification/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.android.identityverification;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener {
 
+    FragmentManager fragmentManager;
+    Fragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();  fragmentManager = getSupportFragmentManager();
+        fragment= new HomeFragment();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setCustomAnimations(android.R.anim.fade_in,android.R.anim.fade_out);
+        transaction.replace(R.id.container,fragment).commit();
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
